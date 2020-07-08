e86f2d146dfb60229ee0d4501a36d2770d69a5fc
Cyberhustler <62091903+Cyberhustler@users.noreply.github.com>
2020-07-08 10:00:51 +0200

    Create randoweb.py

---
+++ randoweb.py
@@ -0,0 +1,17 @@
+import webbrowser
+import time
+import random
+while True:
+    sites = random.choice(
+        [
+            "google.com",
+            "youtube.com",
+            "twitter.com",
+            "linkedin.com",
+            "facebook.com",
+        ]
+    )
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(1, 3)
+    time.sleep(seconds)
