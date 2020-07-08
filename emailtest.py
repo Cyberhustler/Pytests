080fd84498bf6f272444d0386910e71ca5b80241
Cyberhustler <62091903+Cyberhustler@users.noreply.github.com>
2020-07-08 10:03:07 +0200

    Create emailtest.py

---
+++ emailtest.py
@@ -0,0 +1,82 @@
+from gtts import gTTS
+import speech_recognition as sr
+import os
+import webbrowser
+import smtplib
+SECRET_KEY = os.getenv("DATABASE_EMAIL")
+SECRET_KEY2 = os.getenv("DATABASE_PASSWORD")
+
+
+def talkToMe(audio):
+    print(audio)
+    tts = gTTS(text=audio, lang='en')
+    tts.save('audio.mp3')
+    os.system('mpg123 audio.mp3')
+
+# listen for commands
+
+
+def myCommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('I am ready for your next command')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio)
+        print('You said: ' + command + '/n')
+
+# loop back to continue to listen for commands
+
+    except sr.UnknownValueError:
+        assistant(myCommand())
+
+    return command
+
+# if statements for executing commands
+
+
+def assistant(command):
+
+    if 'open Reddit python' in command:
+        chrome_path = '/usr/bin/google-chrome'
+        url = 'https://www.reddit.com/r/python'
+        webbrowser.get(chrome_path).open(url)
+
+    if 'what\'s up' in command:
+        talkToMe('who is the recipient')
+        recipient = myCommand()
+
+        if 'John' in recipient:
+            talkToMe('What should i say')
+            content = myCommand()
+
+            # init hmal SMTP
+            mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+            # identify to server
+            mail.ehlo()
+
+            # encrypt session
+            mail.starttls()
+
+            # login
+            mail.login(SECRET_KEY, SECRET_KEY2)
+
+            # send message
+            mail.sendmail('#Person Name', 'emailaddress@whatever.com', content)
+
+            # close connection
+            mail.close()
+
+            talkToMe('Email sent')
+
+
+talkToMe('I am ready for your command')
+
+while True:
+    assistant(myCommand())
